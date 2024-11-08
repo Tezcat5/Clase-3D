b3fc953464117d864f80d4a6ec70296c5c1388f9
Tezcat5 <vicpero96@gmail.com>
2024-11-08 10:10:23 +0100

    Minion mueve cajas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
     //--------------Componentes--------------------
     private CharacterController _controller;
     private Transform _camera;
+
     //--------------Inputs-------------------------
-    
     private float _horizontal;
 
     private float _vertical;
     
     [SerializeField] private float _movementSpeed = 5;
+    [SerializeField] private float _pushForce = 10;
 
     private float _turnSmoothVelocity;
 
@@ -30,6 +31,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _sensorRadius = 0.5f;
     [SerializeField] LayerMask _groundLayer;
     [SerializeField] private bool _isGrounded;
+
+    private Vector3 moveDirection;
 
     void Awake()
     {
@@ -62,6 +65,11 @@ public class PlayerController : MonoBehaviour
         }
 
         Gravity();
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            RayTest();
+        }
     }
 
     void AimMovement()
@@ -75,7 +83,7 @@ public class PlayerController : MonoBehaviour
         
         if(direction != Vector3.zero)
         {
-            Vector3 moveDirection =  Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            moveDirection =  Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
             _controller.Move(moveDirection * _movementSpeed * Time.deltaTime);
         }
@@ -87,15 +95,15 @@ public class PlayerController : MonoBehaviour
 
         if(direction != Vector3.zero)
         {
-             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
 
-             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
 
-             transform.rotation = Quaternion.Euler(0, smoothAngle, 0); 
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0); 
 
-             Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
-             _controller.Move(moveDirection * _movementSpeed * Time.deltaTime);
+            _controller.Move(moveDirection * _movementSpeed * Time.deltaTime);
         }
     }
     
@@ -119,9 +127,66 @@ public class PlayerController : MonoBehaviour
         _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
     }
     
+    /*bool IsGrounded()
+    {
+        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+    }*/
+
     bool IsGrounded()
     {
-        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);;
+        RaycastHit hit;
+        if(Physics.Raycast(_sensorPosition.position, -transform.up, out hit, 2))
+        {
+            if(hit.transform.gameObject.layer == 6)
+            {
+                Debug.DrawRay(_sensorPosition.position, -transform.up * 2, Color.green);
+                return true;
+            }
+            else
+            {
+                Debug.DrawRay(_sensorPosition.position, -transform.up * 2, Color.red);
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.layer == 7)
+        {
+
+        }
+
+        Rigidbody rBody = hit.collider.attachedRigidbody;
+
+        if(rBody != null)
+        {
+            Vector3 pushDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+
+            rBody.velocity = pushDirection * _pushForce / rBody.mass;
+        }
+    }
+
+    void RayTest()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.Log(hit.transform.position);
+            Debug.Log(hit.transform.gameObject.layer);
+
+            if(hit.transform.gameObject.tag == "Enemy")
+            {
+                Enemy enemyScript = hit.transform.gameObject.GetComponent<Enemy>();
+
+                enemyScript.TakeDamage();
+            }
+        }
     }
     
     void OnDrawGizmos()
